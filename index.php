f1664d1e6125bbf153101c8786bb3c520d665c16
francescotroiano98 <francesco-troiano-98@libero.it>
2023-07-04 15:39:27 +0200

    check empty input

--- index.php
+++ index.php
@@ -1,26 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-    <title>Password Generator</title>
-</head>
-<body>
-        <form action="./index.php" method="GET">
-            <input type="number" placeholder='Choose the size of password to generate' name="passwordsize">
+        <title>Password Generator</title>
+    </head>
+        <body>
+                <form action="./index.php" method="GET">
+                    <input type="number" placeholder='Choose the size of password to generate' name="passwordsize">
 
-            <button type="submit">
-                Generate Password
-            </button>
-        </form>
+                    <button type="submit">
+                        Generate Password
+                    </button>
+                </form>
 
-        <?php
-    $passwordsize = $_GET['passwordsize'];
-    var_dump($passwordsize);
-?>
-</body>
+                
+
+                <?php
+
+                    if (empty($_GET['passwordsize'])) {
+                        echo 'Please select a number';
+                    } else {
+
+                        $passwordsize = $_GET['passwordsize'];
+                        var_dump($passwordsize);
+            
+                    }
+                    
+
+                
+                    $characters = ['abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=[]{}|;:,.<>?'];
+                ?>
+        </body>
 </html>
